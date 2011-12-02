cc0578ba2588f440b90e5bbbaba39667b7d2e06e
Sergey Kashperskiy <sk@jgenerator.com>
2011-12-01 21:53:11 -0800

    some stuff

--- push-to-github.sh
+++ push-to-github.sh
@@ -4,7 +4,7 @@ rm `find . -name \*~ -type f | grep -v ./.git`
 t=/tmp/push2github$$.txt
 find . -type f |grep -v ./.git |grep -v ./dependencies > $t
 ls ./dependencies/*{.tar.gz,.sh} >> $t 
-git remote add origin git@github.com:NumCracker/core.git
+#git remote add origin git@github.com:NumCracker/core.git
 git add `cat $t`
 rm $t
 git commit -m 'some stuff'
